97e739d34446501c04d4420ea7ba237005997d6f
Wes Oldenbeuving <narnach@gmail.com>
2009-03-17 11:22:49 +0100

    Actually display the page with sites

--- lib/is_the_website_down.rb
+++ lib/is_the_website_down.rb
@@ -1,11 +1,10 @@
-require 'sinatra'
-
 module IsTheWebsiteDown
   class Website
-    attr_reader :url, :status
+    attr_reader :url, :status, :time
     def initialize(url)
       @url=url
       @status=:unknown
+      @time=Time.now
     end
   end
 end
